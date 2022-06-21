c5981bfafff912a3d88b5ef62045815e8bfdca27
phamhoa110 <phamhoa1102001@gmail.com>
2022-06-21 16:35:00 +0700

    nội dung thay đổi

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+  <footer class="main-footer" style="clear: both;">
+    <strong>Copyright &copy; 2022 <a href="https://lic.haui.edu.vn/vn"></a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+     Nhóm 7
+    </div>
+  </footer>
